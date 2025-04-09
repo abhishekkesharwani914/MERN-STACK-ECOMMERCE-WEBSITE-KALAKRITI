b3bf66b5bdf021f59b4382eaf067fc08eba5c9b9
Abhishek Kesharwani <137091837+abhishekkesharwani914@users.noreply.github.com>
2025-04-09 09:44:24 +0530

    Resolve Issues

--- Backend/server.js
+++ Backend/server.js
@@ -26,7 +26,13 @@ main();
 const allowedOrigins = ["http://localhost:5173","https://kalakriti-mern.vercel.app/","https://mern-stack-ecommerce-website-kalakriti.vercel.app/","https://mern-stack-ecommerce-git-0ff6cc-abhishekkesharwani914s-projects.vercel.app/","https://mern-stack-ecommerce-website-ka-abhishekkesharwani914s-projects.vercel.app/"];
 
 // Use of require module or npm packages
-app.use(cors({ origin: allowedOrigins, credentials:true})); // credentials true means we can send the cookies in the response
+app.use(cors({ origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }, credentials:true})); // credentials true means we can send the cookies in the response
 app.use(express.urlencoded({extended: true }));
 app.use(express.json());
 app.use(cookieParser());
